a724b458dd305d2ac865c4a254f1f9b7dcc1d3a8
Mark Robertson <markrobertson67@gmail.com>
2023-07-19 20:35:00 -0400

    Code

---
+++ generateQuestions.js
@@ -0,0 +1,36 @@
+//  To run   node generateQuestions.js
+
+
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config(); // Load environment variables from .env file
+
+async function generateQuestions() {
+  try {
+    // Set up OpenAI configuration
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY, // Use the API key from environment variable
+    });
+    const openai = new OpenAIApi(configuration);
+
+    // Make API call to generate questions
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Book: Eloquent JavaScript\nQuestion:",
+      max_tokens: 64,
+    });
+
+    
+    // console.log("API Response:", response); // Log the entire response object
+
+    // Extract and display the generated question
+        const generatedQuestion = response.data.choices[0]?.text.trim();
+        console.log(`Generated Question: ${generatedQuestion}`);
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+generateQuestions();
+
+
+
